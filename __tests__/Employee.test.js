97aef854894adb7d51290f8cd13eb1504add69cd
reneenh <reneehamlinteaches@gmail.com>
2022-12-16 18:18:51 -0800

    checking for consistency RE: gitignore

--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -1,1 +1,21 @@
 // Jest tests
+const Employee = require("../lib/Employee");
+
+const employee = new Employee("Yukon Cornelius", 5, "yukonc@northpole.com");
+
+test("This code makes a new employee object", () => {
+  const employee = new Employee();
+  expect(typeof employee).toBe("object");
+});
+
+test("Name property appears correctly", () => {
+  expect(employee.name).toBe("Yukon Cornelius");
+});
+
+test("id property appears correctly", () => {
+  expect(employee.id).toBe(5);
+});
+
+test("email appears correctly", () => {
+  expect(employee.email).toBe("yukonc@northpole.com");
+});
