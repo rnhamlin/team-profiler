bb9beaf3b6253a71dcc7bcbf213c61736cd6c08c
reneenh <reneehamlinteaches@gmail.com>
2022-12-16 17:52:29 -0800

    cleaned up comments for clarity

--- __tests__/Engineer.test.js
+++ __tests__/Engineer.test.js
@@ -3,37 +3,14 @@ const Engineer = require("../lib/Engineer");
 
 const engineer = new Engineer("Rudolph", 4, "rudolph@northpole.com", "rudy1");
 
-test("property this.github is set correctly", () => {
+test("this.github is set correctly", () => {
   expect(engineer.github).toBe("rudy1");
 });
 
-test("method this.gitHub() returns correct value", () => {
+test("this.gitHub() returns correct value", () => {
   expect(engineer.getGitHub()).toBe("rudy1");
 });
 
-test("method this.getRole() returns correct value", () => {
+test("this.getRole() returns correct value", () => {
   expect(engineer.getRole()).toBe("Engineer");
 });
-
-// const Engineer = require("../lib/Engineer.js");
-
-// test("creates an engineer object", () => {
-//   const engineer = new Engineer("Rudolph", 3, "rudy@northpole.com", "rudy01");
-
-//   expect(Engineer.name).toBe("Rudolph");
-//   expect(Engineer.id).toBe("3");
-//   expect(Engineer.email).toBe("rudy@northpole.com");
-//   expect(Engineer.role).toBe("Engineer");
-// });
-
-// test("this.gitHub is set correctly", () => {
-//   expect(Engineer.gitHub).toBe("rudy01");
-// });
-
-// test("this.gitHub returns the correct value", () => {
-//   expect(Engineer.getGitHub()).toBe("rudy01");
-// });
-
-// test("Please I need the this.getRole to return correctly", () => {
-//   expect(Engineer.getRole()).toBe("Engineer");
-// });
