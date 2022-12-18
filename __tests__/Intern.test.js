49ec3cc9850072d16dfcfbbacba8c2c29baa105b
reneenh <reneehamlinteaches@gmail.com>
2022-12-17 20:58:44 -0800

    wrote tests for Intern and Manager

--- __tests__/Intern.test.js
+++ __tests__/Intern.test.js
@@ -1,1 +1,17 @@
 // Jest tests
+const Intern = require("../lib/Intern");
+
+const intern = new Intern(
+  "Winter Warlock",
+  6,
+  "wwarlock@northpole.com",
+  "North Pole University"
+);
+
+test("this.school returns the correct value", () => {
+  expect(intern.getSchool()).toBe("North Pole University");
+});
+
+test("this.getRole() returns the correct value", () => {
+  expect(intern.getRole()).toBe("Intern");
+});
--- __tests__/Manager.test.js
+++ __tests__/Manager.test.js
@@ -1,1 +1,17 @@
 // Jest tests
+const Manager = require("../lib/Manager");
+
+const manager = new Manager(
+  "Mrs. Claus",
+  7,
+  "missusC@northpole.com",
+  "Room 44"
+);
+
+test("getRole() returns the correct value", () => {
+  expect(manager.getRole()).toBe("Manager");
+});
+
+test("getOffice() returns the correct value", () => {
+  expect(manager.getOffice()).toBe("Room 44");
+});
